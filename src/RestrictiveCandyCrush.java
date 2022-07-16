351dd88bc97056420a5ebf27dbda7b32678b90c8
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-16 01:10:20 -0700

    created a pair class to store the characters of string and their frequencies in stack

--- src/RestrictiveCandyCrush.java
+++ src/RestrictiveCandyCrush.java
@@ -1,5 +1,14 @@
 public class RestrictiveCandyCrush {
+
+    static class Pair{
+        char ch;
+        int freq;
+        Pair(char ch, int freq){
+            this.ch = ch;
+            this.freq = freq;
+        }
+    }
     public static void main(String[] args) {
-        
+
     }
 }
