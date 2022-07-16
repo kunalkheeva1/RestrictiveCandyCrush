67472cf3124fdce5bfaa17f31f866a3b9f3c89b3
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-16 01:31:58 -0700

    added the comments for the better understanding

--- src/RestrictiveCandyCrush.java
+++ src/RestrictiveCandyCrush.java
@@ -13,27 +13,37 @@ public class RestrictiveCandyCrush {
     public static String reduced_String(int k, String s){
         if(k==1) return "";
 
+        // initialize a stack of pairs
         Stack<Pair> stack = new Stack<>();
+
+        // loop till the end of the string
       for(int i=0; i<s.length(); i++){
 
-
+        // if stack is empty then add the character with frequency 1
         if(stack.isEmpty()){
             Pair pair = new Pair(s.charAt(i), 1);
             stack.push(pair);
         }
+        //else
         else{
+            // if the element in the stack is not same then add that too
             if(stack.peek().ch != s.charAt(i)){
                 Pair pair = new Pair(s.charAt(i), 1);
                 stack.push(pair);
-            }else{
+            }
+            // if element is same and stack is not empty then increment the frequency
+            else
+            {
                 int frequency = stack.peek().freq;
                 frequency = frequency+1;
-
+                // but if the frequency is not equals to k limit, then add the pair with incremented frequency
                 if(frequency != k){
                     Pair pair = new Pair(s.charAt(i), frequency);
                     stack.push(pair);
-                }else{
-                    frequency --;
+                }// otherwise decrement the frequency and delete that element until its frequency gets 0
+                else
+                {
+                    frequency --;           // idk why did we do this, as we are doing same thing in loop
                     while(frequency>0){
                         stack.pop();
                         frequency--;
@@ -51,7 +61,8 @@ public class RestrictiveCandyCrush {
 
 
 
-    }StringBuilder ans = new StringBuilder();
+    }// append the elements in string builder and then reverse it to get the ans as stack is FILO
+      StringBuilder ans = new StringBuilder();
       ans =ans.append(stack.pop().ch);
       StringBuilder ans2 = new StringBuilder();
       ans2 = ans.reverse();
