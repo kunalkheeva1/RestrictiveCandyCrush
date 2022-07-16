85ebf3670336386dee6b663ce13c94019c57114c
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-16 01:24:03 -0700

    created a method to delete the consecutive k repetitive elements from a given string

--- src/RestrictiveCandyCrush.java
+++ src/RestrictiveCandyCrush.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class RestrictiveCandyCrush {
 
     static class Pair{
@@ -8,6 +10,59 @@ public class RestrictiveCandyCrush {
             this.freq = freq;
         }
     }
+    public static String reduced_String(int k, String s){
+        if(k==1) return "";
+
+        Stack<Pair> stack = new Stack<>();
+      for(int i=0; i<s.length(); i++){
+
+
+        if(stack.isEmpty()){
+            Pair pair = new Pair(s.charAt(i), 1);
+            stack.push(pair);
+        }
+        else{
+            if(stack.peek().ch != s.charAt(i)){
+                Pair pair = new Pair(s.charAt(i), 1);
+                stack.push(pair);
+            }else{
+                int frequency = stack.peek().freq;
+                frequency = frequency+1;
+
+                if(frequency != k){
+                    Pair pair = new Pair(s.charAt(i), frequency);
+                    stack.push(pair);
+                }else{
+                    frequency --;
+                    while(frequency>0){
+                        stack.pop();
+                        frequency--;
+                    }
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+    }StringBuilder ans = new StringBuilder();
+      ans =ans.append(stack.pop().ch);
+      StringBuilder ans2 = new StringBuilder();
+      ans2 = ans.reverse();
+      return ans2.toString();
+    }
+
+
+
+
+
+
     public static void main(String[] args) {
 
     }
